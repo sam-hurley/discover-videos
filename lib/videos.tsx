17d04f1c43c84452085a841f27f2d0469c161565
moricode49 <samhurley4@hotmail.com>
2024-10-08 18:03:13 +0100

    implement popular videos

--- lib/videos.tsx
+++ lib/videos.tsx
@@ -1,12 +1,16 @@
 import videoData from "../data/videos.json";
 
-export const getVideos = async (searchQuery) => {
+export const getCommonVideos = async (url) => {
 	const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
 	try {
+		const BASE_URL = "youtube.googleapis.com/youtube/v3";
+
 		const response = await fetch(
-			`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchQuery}&type=video&key=${YOUTUBE_API_KEY}`
+			`https://${BASE_URL}/${url}&key=${YOUTUBE_API_KEY}`
 		);
+
+		// `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${YOUTUBE_API_KEY}`
 
 		const data = await response.json();
 
@@ -27,4 +31,14 @@ export const getVideos = async (searchQuery) => {
 		console.error("Something went wrong with video library", error);
 		return [];
 	}
+};
+
+export const getVideos = (searchQuery) => {
+	const URL = `search?part=snippet&maxResults=25&q=${searchQuery}&type=video`;
+	return getCommonVideos(URL);
+};
+
+export const getPopularVideos = () => {
+	const URL = `videos?part=snippet&maxResults=25&%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US`;
+	return getCommonVideos(URL);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,23 @@ import styles from "@/styles/Home.module.css";
 import Banner from "@/components/banner/banner";
 import NavBar from "@/components/nav/navbar";
 import SectionCards from "@/components/card/section-cards";
-import { getVideos } from "@/lib/videos";
+import { getPopularVideos, getVideos } from "@/lib/videos";
 
 export async function getServerSideProps() {
 	const disneyVideos = await getVideos("disney trailer");
 	const productivityVideos = await getVideos("productivty");
 	const travelVideos = await getVideos("travel");
-	// const popularVideos = await getVideos("popular");
-	return { props: { disneyVideos, productivityVideos, travelVideos } };
+	const popularVideos = await getPopularVideos();
+	return {
+		props: { disneyVideos, productivityVideos, travelVideos, popularVideos },
+	};
 }
 
 export default function Home({
 	disneyVideos,
 	productivityVideos,
 	travelVideos,
+	popularVideos,
 }) {
 	console.log(disneyVideos);
 	return (
@@ -25,22 +28,23 @@ export default function Home({
 				<title>Netflix</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-
-			<NavBar username="samhurley@gmail.com" />
-			<Banner
-				title={"Clifford the red dog"}
-				subTitle={"a very cute dog"}
-				imgUrl="/static/clifford.webp"
-			/>
-			<div className={styles.sectionWrapper}>
-				<SectionCards title="Disney" videos={disneyVideos} size="large" />
-				<SectionCards title="Travel" videos={travelVideos} size="small" />
-				<SectionCards
-					title="Productivity"
-					videos={productivityVideos}
-					size="medium"
+			<div className={styles.main}>
+				<NavBar username="samhurley@gmail.com" />
+				<Banner
+					title={"Clifford the red dog"}
+					subTitle={"a very cute dog"}
+					imgUrl="/static/clifford.webp"
 				/>
-				<SectionCards title="Popular" videos={disneyVideos} size="small" />
+				<div className={styles.sectionWrapper}>
+					<SectionCards title="Disney" videos={disneyVideos} size="large" />
+					<SectionCards title="Travel" videos={travelVideos} size="small" />
+					<SectionCards
+						title="Productivity"
+						videos={productivityVideos}
+						size="medium"
+					/>
+					<SectionCards title="Popular" videos={popularVideos} size="small" />
+				</div>
 			</div>
 		</div>
 	);
