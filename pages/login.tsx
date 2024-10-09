9091fc93e3214230e4ce6bdd540fe27696c89c5f
moricode49 <samhurley4@hotmail.com>
2024-10-09 09:36:38 +0100

    add sign-in form validation

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,13 +2,29 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Login.module.css";
-
-const handleLoginWithEmail = (e) => {
-	console.log("click");
-	e.preventDefault();
-};
+import { useState } from "react";
 
 export default function Login() {
+	const [email, setEmail] = useState("");
+	const [userMsg, setUserMsg] = useState("");
+
+	const handleOnChangeEmail = (e) => {
+		setUserMsg("");
+		console.log("event", e);
+		const email = e.target.value;
+		setEmail(email);
+	};
+
+	const handleLoginWithEmail = (e) => {
+		console.log("click");
+		e.preventDefault();
+		if (email) {
+			//route to dashboard
+		} else {
+			setUserMsg("Please enter valid email address");
+		}
+	};
+
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -35,8 +51,9 @@ export default function Login() {
 						type="text"
 						placeholder="Email address"
 						className={styles.emailInput}
+						onChange={handleOnChangeEmail}
 					/>
-					<p className={styles.userMsg}>Message</p>
+					<p className={styles.userMsg}>{userMsg}</p>
 					<button onClick={handleLoginWithEmail} className={styles.loginBtn}>
 						Sign In
 					</button>
