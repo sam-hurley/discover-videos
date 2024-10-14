52439a2a20ae9e7d6c3ded7580557a74a4a6698a
moricode49 <samhurley4@hotmail.com>
2024-10-14 10:18:34 +0100

    add modal component

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -1,7 +1,22 @@
 import { useRouter } from "next/router";
+import styles from "../../styles/Video.module.css";
+import Modal from "react-modal";
+Modal.setAppElement("#__next");
 
 export default function Video() {
 	const router = useRouter();
 	console.log(router.query);
-	return <div>video page {router.query.videoId}</div>;
+	return (
+		<div>
+			video page {router.query.videoId}
+			<Modal
+				isOpen={true}
+				contentLabel="Watch the video"
+				onRequestClose={() => router.back()}
+				overlayClassName={styles.overlay}
+			>
+				<div>Modal Body</div>
+			</Modal>
+		</div>
+	);
 }
