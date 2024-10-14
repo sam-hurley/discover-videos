3cb65a2bad33226971ca13ed7f4bf04a4eeae8b9
moricode49 <samhurley4@hotmail.com>
2024-10-14 11:04:44 +0100

    add video player styling

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -16,8 +16,9 @@ export default function Video() {
 			>
 				<iframe
 					id="ytplayer"
+					className={styles.videoPlayer}
 					type="text/html"
-					width="640"
+					width="100%"
 					height="360"
 					src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
 					frameborder="0"
